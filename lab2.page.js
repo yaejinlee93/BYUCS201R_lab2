b69b993650b5f8db49ad8db8250cddbb623577f4
Jordan Nielson <jnielson94@gmail.com>
2017-01-17 17:37:48 -0700

    Updating URL spec

--- lab2.page.js
+++ lab2.page.js
@@ -1,5 +1,6 @@
 var Lab2 = function() {
   var self = this;
+  //Make sure that your URL is in the form: xxx.xxx/labs2/weather.html
   
   self.cityInput = $("#cityField");
   self.suggestionsList = $("#txtHint");
